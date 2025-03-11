960fb4ff41dd8ce43d21892b439c7d6e5bcc81a3
evalieungh <evaleriksen@gmail.com>
2025-03-11 11:29:40 +0100

    improve indentation

--- P6_VegChange.R
+++ P6_VegChange.R
@@ -6,13 +6,16 @@ library(readxl)
 
 # Automatic import
 setwd("C:/Users/"[insert the path to your working directory here]) #Set working directory
-gnmdstot <- read_xls("P6_OppkuvenGNMDS.xls", sheet = "BASIC") %>% as.data.frame() # Importing spreadsheet BASIC from data file P6OppkuvenGNMDS.xls
-sp.change <- read_xls("P6_OppkuvenGNMDS.xls", sheet = "Species change") %>% as.data.frame() # Importing species data
+gnmdstot <- read_xls("P6_OppkuvenGNMDS.xls", 
+                     sheet = "BASIC") %>% 
+  as.data.frame() # Importing spreadsheet BASIC from data file P6OppkuvenGNMDS.xls
+sp.change <- read_xls("P6_OppkuvenGNMDS.xls", 
+                      sheet = "Species change") %>% 
+  as.data.frame() # Importing species data
 
 # Or, via import from Clipboard
 gnmdstot <- read.table("clipboard", header = TRUE)
 sp.change <- read.table("clipboard", header = TRUE)
-
 
 # Attach names to variables
 attach(gnmdstot)
@@ -25,7 +28,6 @@ year <- as.factor(Year)
 year
 stand <- as.factor(Stand)
 stand
-
 
 # Create column for plot ID
 gnmdstot$plot <- rep(x = 1:100, times = 2)
@@ -40,35 +42,49 @@ gnmds_pairs <- gnmdstot %>%
   summarise(x1 = first(GNMDS1), y1 = first(GNMDS2), x2 = last(GNMDS1), y2 = last(GNMDS2))  # Coordinates
 
 # Create a baseline plot for GNMDS
-baseline_plot <- ggplot() + coord_fixed(ratio = 1) + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+baseline_plot <- ggplot() + 
+  coord_fixed(ratio = 1) + 
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
   
 # Plot GNMDS (with plot ID labels)
-baseline_plot + geom_text(data = gnmdstot, aes(x = GNMDS1, y = GNMDS2, label = plot, color = Year), size = 3)
+baseline_plot + 
+  geom_text(data = gnmdstot, 
+            aes(x = GNMDS1, y = GNMDS2, 
+                label = plot, color = Year), 
+            size = 3)
 
 # Plot GNMDS (with points and connective lines)
 baseline_plot + 
-  geom_point(data = gnmdstot, aes(x = GNMDS1, y = GNMDS2, color = Year, size = Year)) +
+  geom_point(data = gnmdstot, 
+             aes(x = GNMDS1, y = GNMDS2, 
+                 color = Year, size = Year)) +
   geom_segment(data = gnmds_pairs, 
-               aes(x = x1, y = y1, xend = x2, yend = y2),
-               arrow = arrow(length = unit(0.2, "cm"), type = "closed"),
+               aes(x = x1, y = y1, 
+                   xend = x2, yend = y2),
+               arrow = arrow(length = unit(0.2, "cm"), 
+                             type = "closed"),
                color = "black")
 
 # Plot GNMDS (with text and connective lines)
 baseline_plot + 
-  geom_text(data = gnmdstot, aes(x = GNMDS1, y = GNMDS2, label = plot, color = Year), size = 3) +
+  geom_text(data = gnmdstot, 
+            aes(x = GNMDS1, y = GNMDS2, 
+                label = plot, color = Year), 
+            size = 3) +
   geom_segment(data = gnmds_pairs, 
-               aes(x = x1, y = y1, xend = x2, yend = y2),
-               arrow = arrow(length = unit(0.2, "cm"), type = "closed"),
+               aes(x = x1, y = y1, 
+                   xend = x2, yend = y2),
+               arrow = arrow(length = unit(0.2, "cm"), 
+                             type = "closed"),
                color = "black")
-
 
 # GNMDS scores (sites) from 97 and 05
 GNMDS1.97 <- GNMDS1[1:100]
 GNMDS2.97 <- GNMDS2[1:100]  
 GNMDS1.05 <- GNMDS1[101:200]
 GNMDS2.05 <- GNMDS2[101:200]
-
-
 
 # How to find mean displacement for some of the axes/stands
 mean(GNMDS1.97)
@@ -87,7 +103,6 @@ a05 <- GNMDS2.05[1:25]
 mean(a05)
 # ...and so on
 
-
 # Explanations for x1, x2 etc. in this table (A-D are the different stands):
 		
 # 1997					
@@ -96,12 +111,9 @@ mean(a05)
 # GNMDS1	x1	0.7925	0.6611	0.6729	0.9188	0.9171	
 # GNMDS2	y1	1.0920	1.2488	0.9987	1.1038	0.9889	
 
-				
 # 2005					
 # GNMDS1	x2	0.7750	0.6421	0.7156	0.8514	0.8911	
 # GNMDS2	y2	1.0975	1.2086	1.0049	1.1100	1.0353	
-
-
 
 # Create data frame for displaying mean displacement from 1997 to 2005
 displacement_data <- data.frame(
@@ -113,17 +125,29 @@ displacement_data <- data.frame(
   stand = factor(x = c("Tot", "A", "B", "C", "D"), levels = c("Tot", "A", "B", "C", "D")))
 
 # Set colors
-stand_colors <- c("Tot" = "blue", "A" = "red", "B" = "green", "C" = "purple", "D" = "orange")
+stand_colors <-
+  c("Tot" = "blue",
+    "A" = "red",
+    "B" = "green",
+    "C" = "purple",
+    "D" = "orange")
 
 # Plot showing mean displacement (try to change arrow parameters: length, angle, 
 # "code, linetype and linewidth, you may even try to change the length of the
 # x and y axes by xlim and ylim)
 ggplot() +
-  geom_point(data = displacement_data, aes(x = x1, y = y1), color = "black", size = 3) +
-  geom_point(data = displacement_data, aes(x = x2, y = y2), color = "black", size = 3) +
+  geom_point(data = displacement_data, 
+             aes(x = x1, y = y1), 
+             color = "black", size = 3) +
+  geom_point(data = displacement_data, 
+             aes(x = x2, y = y2), 
+             color = "black", size = 3) +
   geom_segment(data = displacement_data, 
-               aes(x = x1, y = y1, xend = x2, yend = y2, color = stand),
-               arrow = arrow(length = unit(0.1, "inches"), type = "closed"), linewidth = 1) +
+               aes(x = x1, y = y1, 
+                   xend = x2, yend = y2, 
+                   color = stand),
+               arrow = arrow(length = unit(0.1, "inches"), 
+                             type = "closed"), linewidth = 1) +
   geom_hline(yintercept = 1.0920, color = "black") +
   geom_vline(xintercept = 0.7925, color = "black") +
   labs(x = "GNMDS1", y = "GNMDS2") +
@@ -133,12 +157,6 @@ ggplot() +
   theme_bw() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
-
-
-
-
-
-
 
 # PCA on change in species abundance from 1997 to 2005
 pca.sp <- rda(sp.change, scale = FALSE)
@@ -152,37 +170,40 @@ pca_plot <- ggplot() +
   coord_fixed(ratio = 1) + 
   theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-
 # PCA ordination of species change matrix, site score vectors 
 # Choose scaling = 1 for optimising fit between distances between plots in diagram and floristic dissimilarity
-pca_sites <- scores(pca.sp, display = "sites", scaling = 1) %>% as.data.frame()
+pca_sites <- scores(pca.sp, display = "sites", scaling = 1) %>% 
+  as.data.frame()
 
 # Allocating all sites names to "labels":
 pca_sites$labels <- rownames(sp.change)
 pca_sites$labels
 
 # Plot site scores:
-pca_plot + geom_text(data = pca_sites, aes(x = PC1, y = PC2, label = labels), size = 3)
-
+pca_plot + 
+  geom_text(data = pca_sites, 
+            aes(x = PC1, y = PC2, 
+                label = labels), 
+            size = 3)
 
 # PCA ordination of species change matrix, species score vectors 
 # Choose scaling = 2
-pca_species <- scores(pca.sp, display = "species", scaling = 2) %>% as.data.frame()
+pca_species <- scores(pca.sp, display = "species", scaling = 2) %>% 
+  as.data.frame()
 
 # Allocating all species names to "labels":
 pca_species$labels <- names(sp.change)
 pca_species$labels
 
 # Plot species scores:
-pca_plot + geom_text(data = pca_species, aes(x = PC1, y = PC2, label = labels), size = 3)
-
-
+pca_plot + 
+  geom_text(data = pca_species, 
+            aes(x = PC1, y = PC2, 
+                label = labels), 
+            size = 3)
 
 #### The absolute abundance change ####
 sort(apply(sp.change, 2, function(x) sum(abs(x))))
-
-
-
 
 # Exercise:
 # Perform correlation tests of PCA-axes from species change against environmental 
